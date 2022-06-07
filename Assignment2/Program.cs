5062707ec024df68cbd7cb111a7eb4c0c8ab73b2
jusmims0 <102726528+jusmims0@users.noreply.github.com>
2022-06-07 08:47:36 +0200

    Add project files.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static bool isVowel(char vowel)
+        {
+
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            bool result = false;
+
+            foreach (char eachvowel in vowels)
+            {
+                if (eachvowel == vowel)
+                    result = true;
+            }
+            return result;
+
+        }
+
+
+
+            static string RemoveVowels(string vowel)
+            {
+                string outputString = string.Empty;
+
+                for (int i = 0; i < vowel.Length; i++)
+                {
+                    if (isVowel(vowel[i]))
+                    {
+                        outputString = outputString + "";
+                    }
+                    else
+                    {
+                        outputString = outputString + vowel[i];
+                    }
+
+
+                    
+                }
+                    return outputString;
+        }
+
+              static void Main(string[] args)
+                {
+                    string inputString = "Meisha Leigh Loff ";
+                    
+
+                    string outputString = RemoveVowels(inputString);
+                    Console.WriteLine(outputString);
+
+                }
+            }
+        }
+    
+
+
